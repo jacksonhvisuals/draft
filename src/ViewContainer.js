5c48a3b67e4f821dd4a6734d3a58b905399c0c25
Jackson Hayes <asecretgate@gmail.com>
2018-03-01 19:56:11 -0700

    :bug: fix overflow with Grid

--- src/ViewContainer.js
+++ src/ViewContainer.js
@@ -5,14 +5,16 @@ import './ViewContainer.css';
 export class ViewContainer extends Component {
   render() {
     return (
-      <Row>
-        <Col className='listPane' xs={12} sm={4} md={3}>
-        // Render listPane here
-        </Col>
-        <Col className='detailPane hidden-xs' xs={0} sm={8} md={9}>
-        // Render optional dialog-view here
-        </Col>
-      </Row>
+      <Grid>
+        <Row>
+          <Col className='listPane' xs={12} sm={4} md={3}>
+          // Render listPane here
+          </Col>
+          <Col className='detailPane hidden-xs' xs={0} sm={8} md={9}>
+          // Render optional dialog-view here
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
