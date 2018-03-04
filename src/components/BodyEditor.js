85dbff5034edf84e3068385c32a60da3f3860529
Jackson Hayes <asecretgate@gmail.com>
2018-03-04 15:52:50 -0700

    :sparkles: Add a second text field for Title & enable rich text for body

--- src/components/BodyEditor.js
+++ src/components/BodyEditor.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 export class BodyEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   }
   render() {
     return (
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        <Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange} />
     );
   }
 }
--- src/components/EditorContainer.js
+++ src/components/EditorContainer.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './styles/EditorContainer.css';
 import {BodyEditor} from './BodyEditor';
-
+import {TitleEditor} from './TitleEditor';
 export class EditorContainer extends Component {
   render() {
     return(
       <div className='editor-container-element'>
+        <TitleEditor />
         <BodyEditor />
       </div>
     );
