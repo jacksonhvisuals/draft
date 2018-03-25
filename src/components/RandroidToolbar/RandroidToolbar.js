d77cafe88223234393c5352a715f74604e66cdf7
Jackson Hayes <asecretgate@gmail.com>
2018-03-24 17:03:37 -0700

    :lipstick: add the divider for the toolbar

--- src/components/RandroidToolbar/RandroidToolbar.js
+++ src/components/RandroidToolbar/RandroidToolbar.js
@@ -6,8 +6,12 @@ export class RandroidToolbar extends Component {
   render() {
     return (
       <div className='randroid-toolbar-container'>
-        <Col className='listBar' xs={12} sm={4} md={3}><div className='app-title noselect'>Notes</div></Col>
-        <Col className='detailBar hidden-xs' xs={0} sm={8} md={9}></Col>
+        <Col className='listBar' xs={12} sm={4} md={3}>
+          <div className='app-title noselect'>Notes</div>
+          <div className='toolbar-divider hidden-xs'></div>
+        </Col>
+        <Col className='detailBar hidden-xs' xs={0} sm={8} md={9}>
+        </Col>
       </div>
     );
   }
